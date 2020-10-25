e8816c442b07ddd9b5b617544e82f8aaa24038fc
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2020-10-26 00:34:48 +0100

    Bump Robust.Shared.AuthLib to enforce minimum name length.

--- SS14.Auth.Shared/SS14UserValidator.cs
+++ SS14.Auth.Shared/SS14UserValidator.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Robust.Shared.AuthLib;
 using SS14.Auth.Shared.Data;
 
@@ -11,6 +12,13 @@ namespace SS14.Auth.Shared
 {
     public sealed class SS14UserValidator : UserValidator<SpaceUser>
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public SS14UserValidator(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public override async Task<IdentityResult> ValidateAsync(UserManager<SpaceUser> manager, SpaceUser user)
         {
             var baseResult = await base.ValidateAsync(manager, user);
@@ -22,22 +30,30 @@ namespace SS14.Auth.Shared
             {
                 // Don't run our email checks if even ASP.NET's simpler checks fail.
                 // Otherwise there would be a duplicate "invalid email" message.
-                ValidateEmailAsync(user, errors);
+                await ValidateEmailAsync(user, errors);
             }
 
             return errors.Count > 0 ? IdentityResult.Failed(errors.ToArray()) : IdentityResult.Success;
         }
 
-        private void ValidateEmailAsync(SpaceUser user, List<IdentityError> errors)
+        private async Task ValidateEmailAsync(SpaceUser user, List<IdentityError> errors)
         {
             var email = user.Email;
 
+            string domain;
             try
             {
                 // TODO: .NET 5 has a Try* version of this, switch to that when .NET 5 is available.
-                var _ = new MailAddress(email);
+                var parsedEmail = new MailAddress(email);
+                domain = parsedEmail.Host;
             }
             catch (FormatException)
+            {
+                errors.Add(Describer.InvalidEmail(email));
+                return;
+            }
+
+            if (await _dbContext.BurnerEmails.AnyAsync(p => p.Domain == domain))
             {
                 errors.Add(Describer.InvalidEmail(email));
             }
@@ -58,7 +74,8 @@ namespace SS14.Auth.Shared
                 {
                     // Pretty sure the empty case is already checked by the parent but whatever.
                     UsernameHelpers.UsernameInvalidReason.Empty => "Username cannot be empty.",
-                    UsernameHelpers.UsernameInvalidReason.TooLong => "Username is too long.",
+                    UsernameHelpers.UsernameInvalidReason.TooShort => $"Username is too short (min {UsernameHelpers.NameLengthMin} chars).",
+                    UsernameHelpers.UsernameInvalidReason.TooLong => $"Username is too long (max {UsernameHelpers.NameLengthMax} chars).",
                     UsernameHelpers.UsernameInvalidReason.InvalidCharacter => "Username contains invalid character.",
                     _ => "Unknown error."
                 }
