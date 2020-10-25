abf108084b0d2137a42518eb1779e9f5ff6b9df3
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2020-10-26 00:17:16 +0100

    Don't update username with email address.

--- SS14.Web/Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
+++ SS14.Web/Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
@@ -48,15 +48,6 @@ namespace SS14.Web.Areas.Identity.Pages.Account
                 return Page();
             }
 
-            // In our UI email and user name are one and the same, so when we update the email
-            // we need to update the user name.
-            var setUserNameResult = await _userManager.SetUserNameAsync(user, email);
-            if (!setUserNameResult.Succeeded)
-            {
-                StatusMessage = "Error changing user name.";
-                return Page();
-            }
-
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Thank you for confirming your email change.";
             return Page();
