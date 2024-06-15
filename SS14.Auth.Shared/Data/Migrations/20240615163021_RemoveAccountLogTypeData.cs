8e8e35ba82a7ff238fa76476ef5ca62c57720c64
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2024-06-15 18:50:33 +0200

    Add migrations to remove "Type" field from audit log data.

---
+++ SS14.Auth.Shared/Data/Migrations/20240615163021_RemoveAccountLogTypeData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SS14.Auth.Shared.Data.Migrations
+{
+    public partial class RemoveAccountLogTypeData : Migration
+    {
+        // There was a bug where the "Type" field of the AccountLogEntry type was getting saved into the data too.
+        // This cleans those out as that's silly.
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"AccountLog\" SET \"Data\" = \"Data\" #- '{\"Type\"}';");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ SS14.ServerHub.Shared/Migrations/20240615163140_RemoveAuditLogTypeData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SS14.ServerHub.Shared.Migrations
+{
+    public partial class RemoveAuditLogTypeData : Migration
+    {
+        // There was a bug where the "Type" field of the HubAuditEntry type was getting saved into the data too.
+        // This cleans those out as that's silly.
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"HubAudit\" SET \"Data\" = \"Data\" #- '{\"Type\"}';");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
