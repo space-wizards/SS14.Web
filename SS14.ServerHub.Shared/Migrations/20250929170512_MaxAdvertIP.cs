7006804abb650a239b2e72b31d516409013ff626
Vasilis The Pikachu <vasilis@pikachu.systems>
2025-09-29 19:51:33 +0200

    DB

---
+++ SS14.ServerHub.Shared/Migrations/20250929170512_MaxAdvertIP.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SS14.ServerHub.Shared.Migrations
+{
+    public partial class MaxAdvertIP : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsExceptFromMaxAdvertisements",
+                table: "TrackedCommunity",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsExceptFromMaxAdvertisements",
+                table: "TrackedCommunity");
+        }
+    }
+}
