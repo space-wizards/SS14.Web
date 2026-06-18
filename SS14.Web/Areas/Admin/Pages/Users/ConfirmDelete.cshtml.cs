b687bd5c3cf89bcae2b26ebb49ff01487581803c
juliangiebel <juliangiebel@live.de>
2026-06-18 22:34:07 +0200

    Implement adding deleted user entry when user gets deleted by admin

--- SS14.Web/Areas/Admin/Pages/Users/ConfirmDelete.cshtml.cs
+++ SS14.Web/Areas/Admin/Pages/Users/ConfirmDelete.cshtml.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using SS14.Auth.Shared.Data;
 
 namespace SS14.Web.Areas.Admin.Pages.Users;
@@ -10,14 +11,16 @@ namespace SS14.Web.Areas.Admin.Pages.Users;
 public class ConfirmDelete : PageModel
 {
     private readonly UserManager<SpaceUser> _userManager;
+    private readonly ApplicationDbContext _context;
 
-    public ConfirmDelete(UserManager<SpaceUser> userManager)
+    public ConfirmDelete(UserManager<SpaceUser> userManager, ApplicationDbContext context)
     {
         _userManager = userManager;
+        _context = context;
     }
 
     public SpaceUser SpaceUser { get; set; }
-        
+
     public async Task<IActionResult> OnGetAsync(Guid id)
     {
         SpaceUser = await _userManager.FindByIdAsync(id.ToString());
@@ -40,6 +43,7 @@ public class ConfirmDelete : PageModel
         }
 
         await _userManager.DeleteAsync(SpaceUser);
+        _context.DeletedUserIds.Add(new DeletedUser { SpaceUserId = SpaceUser.Id, DeletedOn = DateTime.UtcNow });
 
         TempData["StatusMessage"] = "User deleted";
         return RedirectToPage("./Index");
